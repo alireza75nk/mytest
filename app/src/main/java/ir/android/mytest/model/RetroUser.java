c0dfad2308aa95200404d3d0dcdac2009a4775f4
Ali <75ali12reza23@gmail.com>
2020-11-08 11:08:24 +0330

    make User Data Model

---
+++ app/src/main/java/ir/android/mytest/model/RetroUser.java
@@ -0,0 +1,54 @@
+package ir.android.mytest.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RetroUser {
+
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("email")
+    private String email;
+    @SerializedName("token")
+    private String token;
+
+    public RetroUser(Integer id, String name, String email, String token) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
